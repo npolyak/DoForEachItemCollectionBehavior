d24c1aa4af1a895924cb09153bb68097066c1acf
npolyak <nick.polyak@verizon.net>
2017-11-24 16:37:59 -0500

    check of the first test project

---
+++ NP.Paradigms/Behaviors/PrintNotifiablePropertyBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.Paradigms.Behaviors
+{
+    public class PrintNotifiablePropertyBehavior : IStatelessBehavior<INotifyPropertyChanged>
+    {
+        public static PrintNotifiablePropertyBehavior TheInstance { get; } =
+            new PrintNotifiablePropertyBehavior();
+
+        private void NotifyiableObject_PropertyChanged
+        (
+            object sender, 
+            PropertyChangedEventArgs e
+        )
+        {
+            PropertyInfo changedPropertyInfo = 
+                sender
+                    .GetType()
+                    .GetProperty(e.PropertyName);
+
+            object propValue = changedPropertyInfo?.GetValue(sender);
+
+            Console.WriteLine(e.PropertyName + ": " + propValue?.ToString());
+        }
+
+        public void Attach(INotifyPropertyChanged notifyiableObject)
+        {
+            notifyiableObject.PropertyChanged += NotifyiableObject_PropertyChanged;
+        }
+
+        public void Detach(INotifyPropertyChanged notifyiableObject)
+        {
+            notifyiableObject.PropertyChanged -= NotifyiableObject_PropertyChanged;
+        }
+    }
+}
---
+++ TESTS/NP.Tests.SimpleStatelessBehaviorTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using NP.Paradigms.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.Tests.SimpleStatelessBehaviorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTestClass myTestClass = new MyTestClass();
+
+            PrintNotifiablePropertyBehavior.TheInstance.Attach(myTestClass);
+
+            // should print (since Behavior is attached)
+            myTestClass.TheString = "Hello World";
+
+            PrintNotifiablePropertyBehavior.TheInstance.Detach(myTestClass);
+
+            // should not print (since Behavior is detached);
+            myTestClass.TheString = "Bye World";
+        }
+    }
+}
