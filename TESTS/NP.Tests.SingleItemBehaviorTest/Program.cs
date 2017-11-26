c416b3842db0f9f116ae04235b7a54c552aa81d2
npolyak <nick.polyak@verizon.net>
2017-11-26 16:27:23 -0500

    added comments

--- TESTS/NP.Tests.SingleItemBehaviorTest/Program.cs
+++ TESTS/NP.Tests.SingleItemBehaviorTest/Program.cs
@@ -5,17 +5,20 @@ namespace NP.Tests.SingleItemBehaviorTest
     {
         static void Main(string[] args)
         {
+            // create the test class
             MyNotifiablePropsTestClass myTestClass = new MyNotifiablePropsTestClass();
 
+            // create the behavior
             PrintNotifiablePropertyBehavior printNotifiablePropertyBehavior = 
                 new PrintNotifiablePropertyBehavior();
 
-
+            // attach the behavior to the class
             printNotifiablePropertyBehavior.Attach(myTestClass);
 
             // should print (since Behavior is attached)
             myTestClass.TheString = "Hello World";
 
+            // detach the behavior from the class
             printNotifiablePropertyBehavior.Detach(myTestClass);
 
             // should not print (since Behavior is detached);
