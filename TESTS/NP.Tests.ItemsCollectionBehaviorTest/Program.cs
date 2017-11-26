934b05c669e52f413d79e02aa273b2aadae691b9
npolyak <nick.polyak@verizon.net>
2017-11-26 18:43:58 -0500

    added documentation to Program.Main()

--- TESTS/NP.Tests.ItemsCollectionBehaviorTest/Program.cs
+++ TESTS/NP.Tests.ItemsCollectionBehaviorTest/Program.cs
@@ -7,40 +7,52 @@ namespace NP.Tests.ItemsCollectionBehaviorTest
     {
         static void Main(string[] args)
         {
-            MyNotifiableCollectionTestClass collectionTestClass = 
+            // create the observable collection containing class
+            MyNotifiableCollectionTestClass collectionTestClass =
                 new MyNotifiableCollectionTestClass();
 
+            // create item 1.
             MyNotifiablePropsTestClass item1 = new MyNotifiablePropsTestClass();
 
-            collectionTestClass.TheCollection = 
+            // set TheCollection property of the collection
+            // containing class to be an ObservableCollection
+            // that contains item1
+            collectionTestClass.TheCollection =
                 new ObservableCollection<MyNotifiablePropsTestClass>
                 (
                     new MyNotifiablePropsTestClass[] { item1 }
                 );
 
-
-            // verify that item1 is notifiable (should print to console): 
+            // change item1.TheString
+            // since item1 is part of the collection,
+            // it should print to console: 
             item1.TheString = "Item1: Hello World";
 
+            // create item2
             MyNotifiablePropsTestClass item2 = new MyNotifiablePropsTestClass();
 
-            // add to collection
+            // add item2 to the collection
             collectionTestClass.TheCollection.Add(item2);
 
-            // verify that notifiable (should print)
+            // change item2.TheString
+            // since item2 is part of the collection,
+            // it should print to console
             item2.TheString = "Item2: Hello World";
 
-            // remove item2
+            // remove item2 from collection
             collectionTestClass.TheCollection.RemoveAt(1);
 
-            // verify that it is not notifiable any longer
-            // (should not print)
+            // since item2 is no longer
+            // part of the collection
+            // should NOT print to console
             item2.TheString = "Item2: Bye Wordl";
 
             // disconnect the whole collection (i.e. Item1)
             collectionTestClass.TheCollection = null;
 
-            // should not print
+            // since the collection property is null,
+            // nothing should be printed to console
+            // when Item1.TheString is changed.
             item1.TheString = "Item1: Bye World";
         }
     }
