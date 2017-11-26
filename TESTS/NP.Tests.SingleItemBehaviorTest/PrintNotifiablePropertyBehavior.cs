55d8e6b04f01a7c1218609d2bf08ad927a5e8d85
npolyak <nick.polyak@verizon.net>
2017-11-26 16:46:00 -0500

    checkpoint

--- TESTS/NP.Tests.SingleItemBehaviorTest/PrintNotifiablePropertyBehavior.cs
+++ TESTS/NP.Tests.SingleItemBehaviorTest/PrintNotifiablePropertyBehavior.cs
@@ -9,23 +9,28 @@ namespace NP.Tests.SingleItemBehaviorTest
     public class PrintNotifiablePropertyBehavior :
         IStatelessBehavior<INotifyPropertyChanged>
     {
+        // the handler
         private static void NotifyiableObject_PropertyChanged
         (
             object sender, 
             PropertyChangedEventArgs e
         )
         {
+            // pring property name and value to the console
+            // using reflection
             sender.PrintPropValue(e.PropertyName);
         }
 
         public void Attach(INotifyPropertyChanged notifyiableObject)
         {
+            // add the handler
             notifyiableObject.PropertyChanged +=
                 NotifyiableObject_PropertyChanged;
         }
 
         public void Detach(INotifyPropertyChanged notifyiableObject)
         {
+            // remove the handler
             notifyiableObject.PropertyChanged -= 
                 NotifyiableObject_PropertyChanged;
         }
