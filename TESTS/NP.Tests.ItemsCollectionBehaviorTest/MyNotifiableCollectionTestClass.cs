7442656d39526fcf3e0687f8f6b9053de979cf64
npolyak <nick.polyak@verizon.net>
2017-11-26 21:56:51 -0500

    added copyright message

--- TESTS/NP.Tests.ItemsCollectionBehaviorTest/MyNotifiableCollectionTestClass.cs
+++ TESTS/NP.Tests.ItemsCollectionBehaviorTest/MyNotifiableCollectionTestClass.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections;
+﻿// (c) Nick Polyak 2017 - http://awebpros.com/
+// License: Code Project Open License (CPOL) 1.92(http://www.codeproject.com/info/cpol10.aspx)
+//
+// short overview of copyright rules:
+// 1. you can use this framework in any commercial or non-commercial 
+//    product as long as you retain this copyright message
+// 2. Do not blame the author(s) of this software if something goes wrong. 
+// 
+// Also as a courtesy, please, mention this software in any documentation for the 
+
+using System.Collections;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using NP.Paradigms;
