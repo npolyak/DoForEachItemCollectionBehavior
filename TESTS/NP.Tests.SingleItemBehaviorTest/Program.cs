1f4374b97ab1a31962f7b19925cf12a1616fd8a1
npolyak <nick.polyak@verizon.net>
2017-11-26 21:58:02 -0500

    added copyright message

--- TESTS/NP.Tests.SingleItemBehaviorTest/Program.cs
+++ TESTS/NP.Tests.SingleItemBehaviorTest/Program.cs
@@ -1,4 +1,14 @@
-﻿
+﻿// (c) Nick Polyak 2017 - http://awebpros.com/
+// License: Code Project Open License (CPOL) 1.92(http://www.codeproject.com/info/cpol10.aspx)
+//
+// short overview of copyright rules:
+// 1. you can use this framework in any commercial or non-commercial 
+//    product as long as you retain this copyright message
+// 2. Do not blame the author(s) of this software if something goes wrong. 
+// 
+// Also as a courtesy, please, mention this software in any documentation for the 
+
+
 namespace NP.Tests.SingleItemBehaviorTest
 {
     class Program
